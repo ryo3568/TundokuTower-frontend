63a9491155c6b52d398defd83176f4368e76c065
ryowhale <kkujira619@gmail.com>
2023-03-25 16:34:40 +0900

    フッターのスタイル変更＃34

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,7 +1,8 @@
-
+import logo from "../images/Tenpensyalogo.png"
 const Footer = () => {
     return (
         <footer>
+            <img className="Tenpensya-logo" alt="てんぺん舎のロゴ" src={logo}/>
             <p>©{new Date().getFullYear()} TundokuTower</p>
         </footer>
     )
