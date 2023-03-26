79a202a6c92258cee253079f9674b056456c3244
r-yanagimoto <r-yanagimoto@ei.sanken.osaka-u.ac.jp>
2023-03-26 12:14:28 +0900

    💄 本一覧UIの変更#70

--- src/pages/item/readFinished.js
+++ src/pages/item/readFinished.js
@@ -21,15 +21,17 @@ const ReadFinished = () => {
         <div>
             <div>
                 <h1>読了本一覧</h1>
-                {finishedItems && finishedItems.finishedItems.map(item => 
-                    <Link to={`/item/single/${item._id}`} key={item._id}>
-                        <img src={item.image} alt="item" />
-                        <div>
-                            <h2>{item.title}</h2>
-                            <h3>{item.author}</h3>
-                        </div>
-                    </Link>
-                )}
+                <div className="books">
+                    {finishedItems && finishedItems.finishedItems.map(item => 
+                        <Link to={`/item/single/${item._id}`} key={item._id}>
+                            <img className="books-img" src={item.image} alt="item" />
+                            <div>
+                                <h2>{item.title}</h2>
+                                <h3>{item.author}</h3>
+                            </div>
+                        </Link>
+                    )}
+                </div>
             </div>
         </div>
     )
